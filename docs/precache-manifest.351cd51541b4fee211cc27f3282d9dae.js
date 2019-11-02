f989a2f393ae42de11bd5c181babac7213325bb4
Mariasola <mariasolahornedo@gmail.com>
2019-11-02 20:12:06 +0100

    buidl docs

---
+++ docs/precache-manifest.351cd51541b4fee211cc27f3282d9dae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732b7140aeb37f9e4297968b6d090eb7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5efbfaee89c6787bad9",
+    "url": "./static/css/main.aa6c509e.chunk.css"
+  },
+  {
+    "revision": "59e73faba64606e7c9da",
+    "url": "./static/js/2.f36c209e.chunk.js"
+  },
+  {
+    "revision": "d5efbfaee89c6787bad9",
+    "url": "./static/js/main.74864de2.chunk.js"
+  },
+  {
+    "revision": "473618ef79ecb8c5f44d",
+    "url": "./static/js/runtime-main.3c520003.js"
+  }
+]);
